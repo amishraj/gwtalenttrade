5bbe6b2f3d75f16ef0c1c2ceaee5e9f01c4a4208
Amish Raj <amishraj@Amishs-MacBook-Pro.local>
2023-11-26 21:14:32 -0500

    minor auth changes

--- app/src/main/java/com/example/gwtalenttrade/MainActivity.java
+++ app/src/main/java/com/example/gwtalenttrade/MainActivity.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button logoutBtn;
+    FirebaseUser user;
+    TextView nameUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         logoutBtn= findViewById(R.id.logoutBtn);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        nameUser= findViewById(R.id.nameUser);
+
+        if(user==null){
+            startActivity(new Intent(getApplicationContext(), firstPage.class));
+            finish();
+        } else{
+            nameUser.setText(user.getEmail());
+        }
 
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
